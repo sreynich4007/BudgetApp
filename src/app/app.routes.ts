db4f64e61462431236fe9cb98c243908dd211bff
Sreynich-Douch <nichdouch@gmail.com>
2026-03-17 09:28:43 +0700

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,75 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BaseChartDirective } from 'ng2-charts';
 
-export const routes: Routes = [];
+// Angular Material Imports
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+
+// App Components
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { BudgetsComponent } from './components/budgets/budgets.component';
+import { AddTransactionModalComponent } from './components/transactions/add-transaction-modal/add-transaction-modal.component';
+
+// Service
+import { FinanceDataService } from './services/finance-data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    DashboardComponent,
+    TransactionsComponent,
+    BudgetsComponent,
+    AddTransactionModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AppRoutingModule,
+    BaseChartDirective, // ng2-charts directive
+
+    // Material Modules
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatTabsModule
+  ],
+  providers: [FinanceDataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
