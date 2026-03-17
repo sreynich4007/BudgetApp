a543ea3e4c00dbefa185a5e554b67d1adc2d4612
DUCH Sreynich <duch.sreynich@itlink.com.kh>
2026-03-17 09:19:13 +0700

    Initial commit - Budget Expense

---
+++ src/app/components/transactions/add-transaction-modal/add-transaction-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-transaction-modal',
+  imports: [],
+  templateUrl: './add-transaction-modal.component.html',
+  styleUrl: './add-transaction-modal.component.css'
+})
+export class AddTransactionModalComponent {
+
+}
